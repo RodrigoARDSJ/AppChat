dece742905eb9bb5fc6f602636625501248fde22
RodrigoRezendeDS2U <rodrigo.rezende@ds2u.com.br>
2022-02-08 19:39:58 -0300

    Alteração no layout do nome nas mensagens

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -9,6 +9,11 @@ var connectingElement = document.querySelector('#connecting');
 var stompClient = null;
 var username = null;
  
+var colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
+
 
 function connect() {
     username = document.querySelector('#username').innerText.trim();
@@ -68,16 +73,23 @@ function onMessageReceived(payload) {
     } else if (message.type === 'DISCONNECT') {
         messageElement.classList.add('event-message');
         message.content = message.sender + 'Saiu!';
-    } else {
+    } else {	
         messageElement.classList.add('chat-message');   
-        var usernameElement = document.createElement('strong');
-        usernameElement.classList.add('nickname');
+
+		var avatarElement = document.createElement('i');
+        var avatarText = document.createTextNode(message.sender[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+
+        messageElement.appendChild(avatarElement);
+
+        var usernameElement = document.createElement('span');
         var usernameText = document.createTextNode(message.sender);
         usernameElement.appendChild(usernameText);
         messageElement.appendChild(usernameElement);
     }
 
-    var textElement = document.createElement('span');
+    var textElement = document.createElement('p');
     var messageText = document.createTextNode(message.content);
     textElement.appendChild(messageText);
 
@@ -85,7 +97,15 @@ function onMessageReceived(payload) {
 
     messageArea.appendChild(messageElement);
     messageArea.scrollTop = messageArea.scrollHeight;
+} 
+
+function getAvatarColor(messageSender) {
+    var hash = 0;
+    for (var i = 0; i < messageSender.length; i++) {
+        hash = 31 * hash + messageSender.charCodeAt(i);
+    }
+    var index = Math.abs(hash % colors.length);
+    return colors[index];
 }
- 
- 
+
 messageForm.addEventListener('submit', sendMessage, true);
